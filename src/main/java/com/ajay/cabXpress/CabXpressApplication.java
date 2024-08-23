3c509149047c1152046fc255957fdaf847a5434d
ajayv28 <ajayvengateshbabu@gmail.com>
2024-08-23 20:30:58 +0300

    1st empty push

---
+++ src/main/java/com/ajay/cabXpress/CabXpressApplication.java
@@ -0,0 +1,13 @@
+package com.ajay.cabXpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabXpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabXpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajay/cabXpress/CabXpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajay.cabXpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabXpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
