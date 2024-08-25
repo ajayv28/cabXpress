e17d5e155ece8edd0255264f37e41ad123b987fe
ajayv28 <ajayvengateshbabu@gmail.com>
2024-08-25 17:02:59 +0300

    Updated DriverRepository

--- src/main/java/com/ajay/cabXpress/repository/DriverRepository.java
+++ src/main/java/com/ajay/cabXpress/repository/DriverRepository.java
@@ -9,4 +9,22 @@ public interface DriverRepository extends JpaRepository<Driver, Integer> {
 
     public Driver findByMobileNumber(long mobileNumber);
 
+    @Query(value = "select * from driver where bookings.size() >= :n", nativeQuery=true)
+    public List<Driver> getDriverWithMoreThanNBooking(int n);
+
+    @Query(value="select * from driver where age >=:n", nativeQuery=true)
+    public List<Driver> getAllDriverByAgeAboveN(int n);
+
+    @Query(value="select * from driver where gender =:gender", nativeQuery=true)
+    public List<Driver> getAllDriverByGender(Gender gender);
+
+    @Query(value="select * from driver where gender =:gender and age <=:n", nativeQuery=true)
+    public List<Driver> getAllDriverByGenderAndAgeBelowN(Gender gender, int n);
+
+    @Query(value="select * from driver where cab <> null", nativeQuery=true)
+    public List<Driver> getAllDriverWithCabRegistered();
+
+    @Query(value="select * from driver where cab = null", nativeQuery=true)
+    public List<Driver> getAllDriverWithNoCabRegistered();
+
 }
