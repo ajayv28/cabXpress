d9acc4760548f889f5fe041913306db015046cee
ajayv28 <ajayvengateshbabu@gmail.com>
2024-08-25 18:06:15 +0300

    Updated CustomerRepository

--- src/main/java/com/ajay/cabXpress/repository/CustomerRepository.java
+++ src/main/java/com/ajay/cabXpress/repository/CustomerRepository.java
@@ -7,4 +7,30 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     public Customer findByEmail(String email);
+
+   /* @Query(value="select * from booking where customer in (select * from customer where id =:id)", nativeQuery=true)
+    public List<Booking> getAllBookingOfCurrentCustomer(int id)
+
+    create customer id in booking */
+
+    /*
+    @Query(value="", nativeQuery=true)
+    public List<Customer> getLastNBookingOfCurrentCustomer(int id)
+    create customer id in booking */
+    
+    @Query(value="select * from customer where registeredOn >=:date", nativeQuery=true)
+    public List<Customer> getAllCustomerRegisteredAfterSpecificDate(Date date);
+    
+    @Query(value="select * from customer where id in (select id from booking group by id having count(*) >= 5 )", nativeQuery=true)
+    public List<Customer> getCustomerWithMoreThanNBooking(int n);
+
+    @Query(value="select * from customer where age >= :n", nativeQuery=true)
+    public List<Customer> getAllCustomerByAgeAboveN(int n);
+
+    public List<Customer> findByGender(Gender gender);
+
+    @Query(value="select * from customer where gender =:gender and age <=:n", nativeQuery=true)
+    public List<Customer> getAllCustomerByGenderAgeBelowN(Gender gender, int n);
+
+    
 }
