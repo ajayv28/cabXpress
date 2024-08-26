f5ab62bf6d2fd5fe28f17d615174a5c959949b20
ajayv28 <ajayvengateshbabu@gmail.com>
2024-08-26 10:39:52 +0300

    Updated Booking Repository Query

--- src/main/java/com/ajay/cabXpress/repository/BookingRepository.java
+++ src/main/java/com/ajay/cabXpress/repository/BookingRepository.java
@@ -13,29 +13,29 @@ import java.util.Date;
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
-    @Query(value="select * from booking order by desc limit :n", nativeQuery=true)
+    @Query(value="select * from booking order by id desc limit :n", nativeQuery=true)
     public List<Booking> getLastNBooking(int n);
 
-    @Query(value="select * from booking where totalFare >=:x", nativeQuery=true)
+    @Query(value="select * from booking where total_fare >=:x", nativeQuery=true)
     public List<Booking> getBookingWithTotalFareGreaterThanX(float x);
 
-    @Query(value="select * from booking where bookingId =:bookingId", nativeQuery=true)
+    @Query(value="select * from booking where bookingId =:booking_id", nativeQuery=true)
     public Booking getBookingDetailWithBookingId(UUID bookingId);
 
-    @Query(value="select * from booking where bookingStatus = CANCELLED", nativeQuery=true)
+    @Query(value="select * from booking where booking_status = 'CANCELLED'", nativeQuery=true)
     public List<Booking> getAllCancelledBooking();
 
-    @Query(value="select * from booking where totalDistance >=:x", nativeQuery=true)
+    @Query(value="select * from booking where total_distance >=:x", nativeQuery=true)
     public List<Booking> getBookingWithTotalDistanceGreaterThanX(double x);
 
-    @Query(value="select * from booking where cabType =:cabType", nativeQuery=true)
+    @Query(value="select * from booking where cab_type =:cabType", nativeQuery=true)
     public List<Booking> getBookingWithGivenCabType(CabType cabType);
 
-    @Query(value="select * from booking where bookingDateAndTime >=:date", nativeQuery=true)
+    @Query(value="select * from booking where booking_date_and_time >=:date", nativeQuery=true)
     public List<Booking> getBookingAfterGivenDate(Date date);
-    // need to convert uti to sql format
+    // need to convert util to sql format
 
-    @Query(value="select * from booking where bookingDateAndTime >=:fromDate and bookingDateAndTime <=:toDate", nativeQuery=true)
+    @Query(value="select * from booking where booking_date_and_time between :fromDate and :toDate", nativeQuery=true)
     public List<Booking> getBookingBetweenGivenDates(Date fromDate, Date toDate);
-    // need to convert uti to sql format
+    // need to convert util to sql format
 }
