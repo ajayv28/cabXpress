eb707ee0e47089702048f08aede6d751379f6df4
ajayv28 <ajayvengateshbabu@gmail.com>
2024-08-26 15:04:00 +0300

    Updated booking service with email confirmation feature

--- src/main/java/com/ajay/cabXpress/service/BookingService.java
+++ src/main/java/com/ajay/cabXpress/service/BookingService.java
@@ -37,6 +37,33 @@ public class BookingService {
     @Autowired
     DriverRepository driverRepository;
 
+    @Autowired
+    JavaMailSender javaMailSender;
+
+    private void sendMailToCustomer (Booking savedBooking) {
+        String text = "Dear Mr./Mrs. " + savedBooking.getCustomer().getName() + ", your booking with cabXpress is confirmed. Cab number - " + savedBooking.getDriver().getCab().getCabNo() + " is on the way to pick you up. Your booking ID is: " + savedBooking.getBookingId() + ". Our Driver " + savedBooking.getDriver().getName() + " is the captain of your trip. Mobile Number of the allocated driver is " + savedBooking.getDriver().getMobileNumber() + ". Thanks for booking with us. Wishing you a safe and happy journey. Kindly call our 24x7 support hotline for any assistance";
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("noreply.cabxpress@gmail.com");
+        simpleMailMessage.setTo(savedBooking.getCustomer().getEmailId());
+        simpleMailMessage.setSubject("cabXpress - Booking Confirmation");
+        simpleMailMessage.setText(text);
+
+        javaMailSender.send(simpleMailMessage);
+    }
+
+
+        private void sendMailToDriver (Booking savedBooking) {
+        String text = "Dear Mr./Mrs. " + savedBooking.getDriver().getName() + ", you have a new booking for your Cab number - " + savedBooking.getDriver().getCab().getCabNo() + ". You can pickup the customer from " + savedBooking.getPickup() + " and drop them at " +  savedBooking.getDestination() + ". You can collect a total fare of Rs." + savedBooking.getTotalFare() + "Incase of any assistance needed with route of pickup location, kindly contact the customer through mobile at" + savedBooking.getDriver().getMobileNumber() + ". Booking ID is: " + savedBooking.getBookingId() +". Kindly maintain the professionalism throughout the ride to ensure highest satisfaction of the guest";
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("noreply.cabxpress@gmail.com");
+        simpleMailMessage.setTo(savedBooking.getDriver().getEmailId());
+        simpleMailMessage.setSubject("cabXpress - New Booking Received for your Cab");
+        simpleMailMessage.setText(text);
+
+        javaMailSender.send(simpleMailMessage);
+    }
 
     public BookingResponse createBooking(BookingRequest bookingRequest) {
 
@@ -68,6 +95,9 @@ public class BookingService {
         driverRepository.save(currDriver);        // cab & booking gets saved as driver has cancading of both
         customerRepository.save(currCustomer);   // booking also will be saved as customer has cascading of booking
 
+        sendMailToCustomer(savedBooking);
+        sendMailToDriver(savedBooking);
+        
         return BookingTransformer.bookingToBookingResponse(savedBooking);
 
 
